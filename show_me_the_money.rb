837a7f7d14de4ba25845f39cbc98f04f0067c189
David Falusi <falusi.david94@gmail.com>
2018-12-13 18:58:49 +0100

    Fix typos in show me the money

--- show_me_the_money.rb
+++ show_me_the_money.rb
@@ -4,11 +4,11 @@ require_relative 'models/account'
 require_relative 'models/bank'
 require_relative 'models/transfer'
 
-class ShowMeThwMoney
+class ShowMeTheMoney
   def initialize
     @bank_a = Bank.new(name: 'A')
     @bank_b = Bank.new(name: 'B')
-    @jims_account = Account.new(user: 'Jim', balance: 25000, bank: @bank_a)
-    @emmas_account = Account.new(user: 'Emma', balance: 15000, bank: @bank_b)
+    @jims_account = Account.new(user: 'Jim', balance: 25_000, bank: @bank_a)
+    @emmas_account = Account.new(user: 'Emma', balance: 15_000, bank: @bank_b)
   end
 end
