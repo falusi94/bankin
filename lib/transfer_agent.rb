dabb91bdc4b965a74689ed1d47be71eb75841a1f
David Falusi <falusi.david94@gmail.com>
2018-12-13 18:34:56 +0100

    Add transfer aget initailizer

--- lib/transfer_agent.rb
+++ lib/transfer_agent.rb
@@ -1,3 +1,12 @@
 # frozen_string_literal: true
 
-class TransferAgent; end
+class TransferAgent
+  attr_accessor :from, :to, :amount, :transfer_limit
+
+  def initialize(params = {})
+    @from = params[:from]
+    @to = params[:to]
+    @amount = params[:amount]
+    @transfer_limit = params[:transfer_limit]
+  end
+end
--- tests/test_transfer_agent.rb
+++ tests/test_transfer_agent.rb
@@ -7,4 +7,13 @@ class TestTransferAgent < Test::Unit::TestCase
   def test_transfer_agent_can_be_created
     TransferAgent.new
   end
+
+  def test_bank_init_parameters_set_properly
+    transfer_agent =
+      TransferAgent.new(from: 'account1', to: 'account2', amount: 500, transfer_limit: 1500)
+    assert_equal 'account1', transfer_agent.from
+    assert_equal 'account2', transfer_agent.to
+    assert_equal 500, transfer_agent.amount
+    assert_equal 1500, transfer_agent.transfer_limit
+  end
 end
